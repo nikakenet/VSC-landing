b966e84f4fe28e38a5dc12718f133013f7c36a1c
Nina Kenetayeva <nina.k@internal.Tabella.cz>
2019-09-04 17:04:09 +0200

    updates links

--- php/getjackpot.php
+++ php/getjackpot.php
@@ -2,7 +2,7 @@
 
 // AJAX calls here for latest jackpots info after single article page is loaded
 
-$jackpot = file_get_contents("https://www.planet7casino.com/jackpots/getjackpot.php");
+$jackpot = file_get_contents("https://www.vegasstripcasino.com/jackpots/getjackpot.php");
 $jackpot = json_decode($jackpot,true);
 $jackpot  =  (string) $jackpot;
 
